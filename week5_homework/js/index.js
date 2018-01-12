67be4d2259cd0e25cecd23a51118fc0a69353023
Jonathan Boswell <jonathan@arborbridge.com>
2018-01-12 09:34:40 -0800

    Bigger Change

--- week5_homework/js/index.js
+++ week5_homework/js/index.js
@@ -1,4 +1,32 @@
 //Declare a variable and assign a value
+var likePet = true;
+
+$('#pets').on('click', yesForPets);
+$('#noPets').on('click', nayForPets);
+
+function yesForPets() {
+    likePet = true;
+    showImage();
+}   
+
+function nayForPets(){
+    likePet = false;
+    showImage();
+}
+
+function showImage()
+{
+    if (likePet) {
+        $('#result').html('PETS ARE THE BEST!!!');
+        $('body').html('<img src="images/bo.jpg" />');
+    } else {
+        $('#conclusion').html("I PREFER MY FREEDOM");
+        $('body').html('<img src="images/debbie.gif" />');    
+    }
+}
+
+/*
+//Declare a variable and assign a value
 var likePet = "true"
 var nayPet = "true"
 
@@ -7,28 +35,18 @@ $('#pets').on('click', yesForPets);
 $('#noPets').on('click', nayForPets);
 
 function yesForPets() {
-	if (likePet == "true") {
+    if (likePet == "true") {
         $('#result').html('PETS ARE THE BEST!!!');
         $('body').html('<img src="images/bo.jpg" />');
     }
-}	
+}   
 
 
 function nayForPets(){
-	if (nayPet == "true")
+    if (nayPet == "true")
     {
         $('#conclusion').html("I PREFER MY FREEDOM");
         $('body').html('<img src="images/debbie.gif" />');
     }
 }
-
-//what code do i use if i want one result to go away when i click on the different button?
-
- //$('#yourButton').on('click', yourFunction);
-//$('#yourButton').on('click', yourFunction);
-//$('#yourButton').on('click', yourFunction);
-
-  //  $('#result').html('Do something');
-  //  console.log("#yourButton was clicked");
-  //else 
-	 //$('body').removeClass('CATS ROCK');
+*/
